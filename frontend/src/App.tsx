ce85517cf0640af123d421fa91271fa0b3e5f58e
bentorodrigues2 <bentorodrgues2@gmail.com>
2026-08-18 01:43:11 +0100

    Ativar React Router e rota /app para AI Studio

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AIStudioWrapper from "./aistudio/AIStudioWrapper";
+
 function App() {
   return (
-    <div>
-      <h1>Frontend OK</h1>
-        <a href="/app">Área Pessoal</a>
-</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>Frontend OK</h1>
+              <a href="/app">Área Pessoal</a>
+            </div>
+          }
+        />
+        <Route path="/app/*" element={<AIStudioWrapper />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
