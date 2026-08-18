3111e7455eb0531db2d3b2f65190abebecd4ac1c
bentorodrigues2 <bentorodrgues2@gmail.com>
2026-08-19 00:49:51 +0100

    Reconstrução completa do frontend

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import "./index.css";
 
 function App() {
   return (
-    <div style={{ padding: "2rem", fontFamily: "system-ui" }}>
-      <h1>Frontend restaurado</h1>
+    <div style={{ padding: "2rem" }}>
+      <h1>Frontend Restaurado</h1>
       <p>O projeto foi reconstruído automaticamente.</p>
     </div>
   );
