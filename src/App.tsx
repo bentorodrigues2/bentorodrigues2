e1aca576d64e3abf5faadd2bb2c13170c45e181f
bentorodrigues2 <bentorodrgues2@gmail.com>
2026-08-18 23:53:40 +0100

    Corrigir import do AI Studio

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <h1>Frontend OK</h1>
-              <a href="/app">Área Pessoal</a>
-            </div>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
-
 import { askAI } from "../aistudio/useAI";
 
 async function testAI() {
