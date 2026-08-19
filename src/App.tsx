b12d67a1dc3b74ab805a5379c2f4db14026a51f8
bentorodrigues2 <bentorodrgues2@gmail.com>
2026-08-19 09:34:18 +0100

    Repor frontend base funcional

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./App.css";
 import "./index.css";
 
 function App() {
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Frontend Restaurado</h1>
-      <p>O projeto foi reconstruído automaticamente.</p>
+    <div className="App">
+      <h1>Frontend restaurado</h1>
+      <p>O projeto voltou ao estado funcional.</p>
     </div>
   );
 }
