9cb4a1fe19ccf1fdc93e273335306a1c2c919bb1
bentorodrigues2 <bentorodrgues2@gmail.com>
2026-08-19 09:55:02 +0100

    Adicionar BrowserRouter e ativar AI Studio

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import AIStudioRouter from "./aistudio/router";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Frontend restaurado</h1>
-      <p>O projeto voltou ao estado funcional.</p>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Frontend restaurado</h1>
+        <p>O projeto voltou ao estado funcional.</p>
 
-      <AIStudioRouter />
-    </div>
+        <AIStudioRouter />
+      </div>
+    </BrowserRouter>
   );
 }
 
