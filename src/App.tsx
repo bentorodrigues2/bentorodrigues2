10082f512f7940569a01942190d0f7b5531486a4
bentorodrigues2 <bentorodrgues2@gmail.com>
2026-08-19 09:47:32 +0100

    Corrigir App.tsx e ativar AI Studio

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./App.css";
 import "./index.css";
+import AIStudioRouter from "./aistudio/router";
 
 function App() {
   return (
     <div className="App">
       <h1>Frontend restaurado</h1>
       <p>O projeto voltou ao estado funcional.</p>
-    <AIStudioRouter /></div>
+
+      <AIStudioRouter />
+    </div>
   );
 }
 
 export default App;
-import AIStudioRouter from "./aistudio/router";
-<AIStudioRouter />
