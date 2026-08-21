8561e739e931cb713ae2044d1ab99f0b9a49f4cf
bentorodrigues2 <bentorodrgues2@gmail.com>
2026-08-21 02:18:30 +0100

    Correção automática do import authServiceModal

--- src/components/LayoutTop.tsx
+++ src/components/LayoutTop.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import AuthModal from "../services/authServiceModal";
+import { } from "../services/authService";
 import "./LayoutTop.css";
 
 interface LayoutTopProps {
