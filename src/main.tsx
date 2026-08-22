24c93d5b4bf69581e2000f271487336a47ae65e9
bentorodrigues2 <bentorodrgues2@gmail.com>
2026-08-22 01:56:50 +0100

    Tailwind fully configured automatically (no regex)

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+﻿import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
@@ -8,3 +9,4 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <RouterProvider router={aiStudioRouter} />
   </React.StrictMode>
 );
+
